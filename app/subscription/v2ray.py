528a751cdc5146d306e91ceac78bff87a8d96b30
Revaz333 <rezobol357@mail.ru>
2026-04-18 15:57:18 +0300

    deleted email from code

--- app/subscription/v2ray.py
+++ app/subscription/v2ray.py
@@ -800,7 +800,7 @@ class V2rayJsonConfig(str):
                         {
                             "id": id,
                             "alterId": 0,
-                            "email": "https://gozargah.github.io/marzban/",
+                            "email": "",
                             "security": "auto"
                         }
                     ],
@@ -820,7 +820,7 @@ class V2rayJsonConfig(str):
                             "id": id,
                             "security": "auto",
                             "encryption": "none",
-                            "email": "https://gozargah.github.io/marzban/",
+                            "email": "",
                             "alterId": 0,
                             "flow": flow
                         }
@@ -837,7 +837,7 @@ class V2rayJsonConfig(str):
                     "address": address,
                     "port": port,
                     "password": password,
-                    "email": "https://gozargah.github.io/marzban/",
+                    "email": "",
                 }
             ]
         }
@@ -850,7 +850,7 @@ class V2rayJsonConfig(str):
                     "address": address,
                     "port": port,
                     "password": password,
-                    "email": "https://gozargah.github.io/marzban/",
+                    "email": "",
                     "method": method,
                     "uot": False,
                 }
